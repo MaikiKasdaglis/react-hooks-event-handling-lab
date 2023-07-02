31c9e16110da61b59d20241f11e5a1c856a2e54b
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-07-02 13:46:15 -0600

    finishes lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+   
+function focus(e) {
+    console.log('Good!')
+}
+function blur(e) {
+    console.log('Hey! Eyes on me!')
+}
+    return (
+        <div>
+            <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
